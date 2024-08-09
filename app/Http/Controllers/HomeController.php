97b5be4e7d56265dc98dfb5411ec4ad861047722
fkoki0309 <fkoki0309@gmail.com>
2024-08-09 14:04:44 +0900

    モデルの追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Category;
 
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = DB::table('categories')->get(); 
+        $allCategories = Category::all(); 
  
         return view('home', ['categories' => $allCategories]); 
     }
