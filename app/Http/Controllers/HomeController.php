49f2d0a005e5a63b6735bccc397948cfb5585763
fkoki0309 <fkoki0309@gmail.com>
2024-08-11 13:50:01 +0900

    postの表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Post;
 
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = Category::all(); 
+        $categories = Category::all(); 
+        $posts = Post::when(request('category_id'), function ($query) { 
+            $query->where('category_id', request('category_id'));
+        })->latest()->get();
  
-        return view('home', ['categories' => $allCategories]); 
+        return view('home', compact('categories','posts')); 
     }
 }
